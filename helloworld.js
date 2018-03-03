702478de3925effaff350ce5afcd729ab5956df5
Jacob Hesch <jacob@muxgram.com>
2018-03-02 18:36:07 -0700

    Add chatbot files

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+// Hello World chatbot
+
+/* global muxbots */
+
+muxbots.onChat((message, callback) => {
+  if (message.toLowerCase() === 'hello') {
+    muxbots.newResponse()
+      .addMessage('Hello! 👋')
+      .send(callback)
+  } else {
+    muxbots.newResponse()
+      .addMessage('Sorry, I only understand one word.')
+      .send(callback)
+  }
+})
